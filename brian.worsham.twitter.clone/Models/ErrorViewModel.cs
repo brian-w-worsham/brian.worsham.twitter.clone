bb1b9d88455a16cae363cae0b7dad31baaccc81b
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-14 22:53:38 -0500

    Add project files.

---
+++ brian.worsham.twitter.clone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace brian.worsham.twitter.clone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
